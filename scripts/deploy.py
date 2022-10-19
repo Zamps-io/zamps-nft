72b8a44b1f0d7058b949327275084f5de727bf0f
felix-mos <felix@momentsofspace.com>
2022-10-19 14:45:09 +0800

    update deploy script

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -5,10 +5,14 @@ from brownie import ZampsToken
 from scripts.helpers import get_deployment_accounts
 
 
-def main():
+def deploy_contract():
     token_uri = os.getenv("TOKEN_URI")  # If this is not set, it will be None
     zamps_account, client_account = get_deployment_accounts()
     zamps_token_contract = ZampsToken.deploy(
         client_account.address, token_uri, {"from": zamps_account}
     )
     return zamps_token_contract
+
+
+def main():
+    deploy_contract()
